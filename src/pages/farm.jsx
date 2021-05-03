4fd71107899e8fc08da1056246727b63aa5c45d5
kumarsuniljat <kumarsuniljat@gmail.com>
2021-05-03 12:38:06 +0530

    removed unnecessary files

--- src/pages/farm.jsx
+++ src/pages/farm.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Button, Dropdown } from 'react-bootstrap';
-import Logo from "../assets/logo.png"
+import React, { useState } from "react";
+import { Container, Row, Col, Button, Dropdown } from "react-bootstrap";
+import Logo from "../assets/logo.png";
 import Footer from "../components/footer";
 import Wallet from "../assets/walletSmall.svg";
 import ConectWallet from "../components/conectWallet";
@@ -9,62 +9,94 @@ import Light from "../assets/light.svg";
 import Dark from "../assets/dark.svg";
 
 const Farm = () => {
-    const [modalShow, setModalShow] = useState(false);
-    const [connectedWallet, setConnectedWallet] = useState(null);
-    const [theme, setTheme] = useState(true);
+  const [modalShow, setModalShow] = useState(false);
+  const [connectedWallet, setConnectedWallet] = useState(null);
+  const [theme, setTheme] = useState(true);
 
-    function onChangeWallet (data) {
-        setConnectedWallet(data);
-        setModalShow(false);
-    }
+  function onChangeWallet(data) {
+    setConnectedWallet(data);
+    setModalShow(false);
+  }
 
-    function onDisconnectWallet () {
-        setConnectedWallet(null);
-        setModalShow(false);
-    }
+  function onDisconnectWallet() {
+    setConnectedWallet(null);
+    setModalShow(false);
+  }
 
-    function changeTheme () {
-        setTheme(!theme);
-    }
+  function changeTheme() {
+    setTheme(!theme);
+  }
 
-    return (
-        <Container fluid className="main_layout">
-                <Row className="header py-4 px-md-5">
-                   <Col xl={12} className="d-flex justify-content-between align-items-center">
-                        <div className="d-flex align-items-center">
-                            <img src={Logo} alt="" /><h4 className="ml-2 text-white mb-0">Farm</h4>
-                        </div>
-                        <div className="d-flex align-items-center">
-                            <Button variant="link" onClick={()=>changeTheme()}><img src={theme ? Light : Dark} /></Button>
-                            {connectedWallet === null ?
-                            <Button variant="light" className="btn_white mx-md-3 h-100" onClick={()=>setModalShow(true)}>Connect Wallet</Button>
-                            :
-                            <Dropdown>
-                                <Dropdown.Toggle variant="" className="btn_white" id="dropdown-basic">
-                                    <img src={Wallet} className="mr-1" />0X13484****79
-                                </Dropdown.Toggle>
+  return (
+    <Container fluid className="main_layout">
+      <Row className="header py-4 px-md-5">
+        <Col
+          xl={12}
+          className="d-flex justify-content-between align-items-center"
+        >
+          <div className="d-flex align-items-center">
+            <img src={Logo} alt="" />
+            <h4 className="ml-2 text-white mb-0">Farm</h4>
+          </div>
+          <div className="d-flex align-items-center">
+            <Button variant="link" onClick={() => changeTheme()}>
+              <img src={theme ? Light : Dark} />
+            </Button>
+            {connectedWallet === null ? (
+              <Button
+                variant="light"
+                className="btn_white mx-md-3 h-100"
+                onClick={() => setModalShow(true)}
+              >
+                Connect Wallet
+              </Button>
+            ) : (
+              <Dropdown>
+                <Dropdown.Toggle
+                  variant=""
+                  className="btn_white"
+                  id="dropdown-basic"
+                >
+                  <img src={Wallet} className="mr-1" />
+                  0X13484****79
+                </Dropdown.Toggle>
 
-                                <Dropdown.Menu>
-                                    <Dropdown.Item href="#" onClick={onDisconnectWallet}>Disconnect</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                            }
-                            <ConectWallet show={modalShow} onHide={() => setModalShow(false)} onChangeWallet={onChangeWallet} themeClass={theme} />
-                        </div>
-                    </Col>
-                </Row>
-                <Row className={`justify-content-center p-5 ${theme ? 'theme_light' : 'theme_dark'}`}>
-                    <Col xl="10" className="">
-                        <h2 className="font-weight-bold">Stake LP tokens to earn PAYR</h2>
-                        <h5>Deposit Fee will be used to buyback PAYR</h5>
-                        <Row className="my-4 justify-content-center justify-content-lg-start">
-                            <FarmCard themeClass={theme} onChangeWallet={onChangeWallet} connectedWallet={connectedWallet} />   
-                        </Row>
-                    </Col>
-                </Row>
-                <Footer themeClass={theme} />
-            </Container>
-    );
-}
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#" onClick={onDisconnectWallet}>
+                    Disconnect
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            )}
+            <ConectWallet
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+              onChangeWallet={onChangeWallet}
+              themeClass={theme}
+            />
+          </div>
+        </Col>
+      </Row>
+      <Row
+        className={`justify-content-center p-5 ${
+          theme ? "theme_light" : "theme_dark"
+        }`}
+      >
+        <Col xl="10" className="">
+          <h2 className="font-weight-bold">Stake LP tokens to earn PAYR</h2>
+          <h5>Deposit Fee will be used to buyback PAYR</h5>
+          <Row className="my-4 justify-content-center justify-content-lg-start">
+            <FarmCard
+              themeClass={theme}
+              onChangeWallet={onChangeWallet}
+              connectedWallet={connectedWallet}
+            />
+          </Row>
+        </Col>
+      </Row>
+      <Footer themeClass={theme} />
+    </Container>
+  );
+};
 
 export default Farm;
--- src/route/index.jsx
+++ src/route/index.jsx
@@ -1,24 +1,18 @@
-import React, { Component } from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-  } from "react-router-dom";
-  
-import Farm from "../pages/farm";
-  
-class Routes extends Component {
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-  
-    render() {
-      return (
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Farm} />
-           </Switch>
-        </Router>
-      );
-    }
+import Farm from "../pages/farm";
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Farm} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default Routes;
