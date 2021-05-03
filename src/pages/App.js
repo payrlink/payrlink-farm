16fe2bcfffc769ef624ab681433d5ee2ae02c7c2
Magic Dev <magicrjh0619@gmail.com>
2021-05-03 10:19:54 +1000

    Revert "add new line"

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,35 +1,10 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Helmet } from "react-helmet";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import AppStore from "../redux/store";
-import PrivateRoute from "../route/PrivateRoute";
-import "../assets/css/fontello.css";
-import "../scss/app.scss";
+import '../scss/App.scss';
+import Route from "../route/index";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Helmet
-          titleTemplate="%s - Blueprint HCP"
-          defaultTitle="Blueprint HCP"
-        >
-          <meta name="description" content="A React.js aapplication" />
-        </Helmet>
-        <Provider store={AppStore.store}>
-          <PersistGate loading={null} persistor={AppStore.persistor}>
-            <div className="App">
-              <Router>
-                <PrivateRoute />
-              </Router>
-            </div>
-          </PersistGate>
-        </Provider>
-      </div>
-    );
-  }
+function App() {
+  return (
+      <Route />
+      );
 }
+
 export default App;
-// export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/action-types.js
+++
@@ -1,9 +0,0 @@
-/**
- * Auh Action Types
- * @type {string}
- */
-export const SET_TOKEN = "SET_TOKEN";
-export const SET_USER = "SET_USER";
-export const SET_FORM = "SET_FORM";
-
-
