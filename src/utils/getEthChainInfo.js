0367d60909460ec725f0de351fffa27965b0a214
Magic Dev <magicrjh0619@gmail.com>
2021-07-12 18:16:05 +1000

    update: migrate to mainnet

--- src/utils/getEthChainInfo.js
+++ src/utils/getEthChainInfo.js
@@ -1,6 +1,8 @@
 export const getEthChainInfo = () => {
-  let chainId = 42;
-  let rpcUrl = 'https://kovan.infura.io/v3/cb76dcac4ae3402fb421b2b0d4c28db3/';
+  // let chainId = 42;
+  // let rpcUrl = 'https://kovan.infura.io/v3/cb76dcac4ae3402fb421b2b0d4c28db3/';
+  let chainId = 1;
+  let rpcUrl = 'https://mainnet.infura.io/v3/cb76dcac4ae3402fb421b2b0d4c28db3/';
   // // if (/\/\/farm.deor.io/.test(href)) {
   //      chainId = 1;
   //      rpcUrl = 'https://mainnet.infura.io/v3/e707b58edfd7437cbb6e9079c259eda7/';
